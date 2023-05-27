25886c373948f2299663c1cedba520b98d3c5295
Keenan Nicholson <Keenanbnicholson@gmail.com>
2023-05-27 16:02:23 -0230

    make sqlite db function // get_rss_feed now takes feeds as an argument

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use rss::Channel;
 use std::error::Error;
 
-async fn example_feed() -> Result<Channel, Box<dyn Error>> {
-    let content = reqwest::get("https://www.cbc.ca/cmlink/rss-canada-newfoundland")
-        .await?
-        .bytes()
-        .await?;
-
+async fn get_rss_feed(feed_url: &str) -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get(feed_url).await?.bytes().await?;
     let channel: Channel = Channel::read_from(&content[..])?;
-
-    channel.write_to(::std::io::sink()).unwrap();
-    let string = channel.to_string();
-
-    println!("{}", string);
-
     Ok(channel)
 }
 
+fn make_db() -> Result<sqlite::Connection, Box<dyn Error>> {
+    let connection: sqlite::Connection = sqlite::open("./rss_feeds.db")?;
+
+    let query: &str = "
+    CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER);
+
+    ";
+    connection.execute(query)?;
+
+    Ok(connection)
+}
+
+//TODO insert channels into db
+fn insert_into_db() {}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    example_feed().await?;
+    let channel = get_rss_feed("https://www.cbc.ca/cmlink/rss-canada-newfoundland").await?;
+    make_db()?;
     Ok(())
 }
